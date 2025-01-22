5cbb5e999befb4ada5deb2bb56a0ee8541c45ca4
Abhishek <abhishekchauhan9119001283@gmail.com>
2025-01-22 07:42:35 +0530

    added new product

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -1,5 +1,6 @@
 import path from "path"
 import {ProductsModel} from "../model/product.model.js"
+import { url } from "inspector"
 export default class ProductController{
     getProducts(req,res){
         const products = ProductsModel.get()
@@ -8,10 +9,27 @@ export default class ProductController{
     //    return res.sendFile(path.join(path.resolve(),"src","view","products.html"))
     }
     addForm(req,res){
-        res.render('new-product',{})
+        res.render('new-product',{errorMessage:null})
     }
     newData(req,res){
-        console.log(req.body)
+        // console.log(req.body)
+        const errors=[]
+        const {name,price,imageUrl} = req.body;
+        if(!name || name.trim() == ''){
+            errors.push("Name is required")
+        }
+        if(!price || price<1){
+            errors.push("Price must be a Positive value")
+        }
+        try{
+            const validUrl = new URL(imageUrl)
+        }catch(err){
+
+            errors.push("Enter a valid URL")
+        }
+        if(errors.length>0){
+            return res.render("new-product",{errorMessage:errors[0]})
+        } 
         ProductsModel.add(req.body)
         const products = ProductsModel.get()
         res.render("products",{products:products})
